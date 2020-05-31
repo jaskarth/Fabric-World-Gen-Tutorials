b7a36747b4f89fdead8526d4960fb744e9a4b2b0
SuperCoder7979 <k.pranav@gmail.com>
2020-05-31 16:17:07 -0400

    fix formatting

--- tutorial 1/src/main/java/supercoder79/tutorialmod/PlainsHillsBiome.java
+++ tutorial 1/src/main/java/supercoder79/tutorialmod/PlainsHillsBiome.java
@@ -11,20 +11,20 @@ public class PlainsHillsBiome extends Biome {
 
 	public PlainsHillsBiome() {
 		super(new Biome.Settings()
-						.configureSurfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_CONFIG)
-						.precipitation(Biome.Precipitation.RAIN)
-						.category(Biome.Category.PLAINS)
-						.depth(0.525F)
-						.scale(0.65F)
-						.temperature(0.8F)
-						.downfall(0.4F)
-						.effects(new BiomeEffects.Builder()
-										.waterColor(4159204)
-										.waterFogColor(329011)
-										.fogColor(12638463)
-										.moodSound(BiomeMoodSound.CAVE).build())
-						.parent(null)
-						.noises(ImmutableList.of(new Biome.MixedNoisePoint(0.0F, 0.0F, 0.0F, 0.0F, 1.0F))));
+			.configureSurfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_CONFIG)
+			.precipitation(Biome.Precipitation.RAIN)
+			.category(Biome.Category.PLAINS)
+			.depth(0.525F)
+			.scale(0.65F)
+			.temperature(0.8F)
+			.downfall(0.4F)
+			.effects(new BiomeEffects.Builder()
+				.waterColor(4159204)
+				.waterFogColor(329011)
+				.fogColor(12638463)
+				.moodSound(BiomeMoodSound.CAVE).build())
+			.parent(null)
+			.noises(ImmutableList.of(new Biome.MixedNoisePoint(0.0F, 0.0F, 0.0F, 0.0F, 1.0F))));
 
 	}
 }
